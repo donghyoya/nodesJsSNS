a3ad3919402c4a54e3cd4b2a48bc55b45a8a71ac
donghyoya <dongwooya123@gmail.com>
2024-02-02 13:42:58 +0900

    setting complete

--- routes/page.js
+++ routes/page.js
@@ -4,6 +4,7 @@ const { renderJoin, renderMain, renderProfile } = require('../controllers/page')
 
 //아래 라우터들에서 공통적으로 사용가능한 변수를 선언 
 router.use((req, res, next) => {
+    // layout.html에 있
     res.locals.user = null;
     res.locals.followerCount = 0;
     res.locals.followingCount = 0;
