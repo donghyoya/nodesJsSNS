f5e45fe757641dae8e44488d4ec1a0b3f9275606
donghyoya <dongwooya123@gmail.com>
2024-02-03 23:45:28 +0900

    re setting indexjs

--- models/index.js
+++ models/index.js
@@ -1,3 +1,38 @@
+const Sequelize = require('sequelize');
+
+const fs = require('fs');
+const path = require('path');
+
+const User = require('./user');
+const Post = require('./post');
+const HashTag = require('./hashtag');
+
+const env = process.env.NODE_ENV || 'development';
+const config = require('../config/config')[env];
+
+const db = {};
+const sequelize = new Sequelize(config.database, config.username, config.password, config, );
+
+
+//sequelize 연결 이때 db라는 객체로 묶었다
+db.sequelize = sequelize;
+db.User = User;
+db.Post = Post;
+db.HashTag = HashTag;
+
+//각 모델안에 initiate, associate 함수를 호출해야한다
+User.initiate(sequelize);
+Post.initiate(sequelize);
+HashTag.initiate(sequelize);
+
+User.associate(db);
+Post.associate(db);
+HashTag.associate(db);
+
+module.exports = db;
+
+
+/* 자동으로 생성된 코드에는 문제가 있기에 주석처리
 'use strict';
 
 const fs = require('fs');
@@ -41,3 +76,5 @@ db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
 module.exports = db;
+
+*/
--- models/user.js
+++ models/user.js
@@ -2,59 +2,59 @@ const Sequelize = require('sequelize');
 
 class User extends Sequelize.Model {
     static initiate(sequelize) {
-        /*
-        email, snsId 가 둘다 null 허용으로 되어있다
-        이는 둘중 하나라도 가지고잇으면 되기 때문이다
-        그
-        */
-        User.init({
             /*
-            속성명: email
-            속성 타입: String(40자 이내)
-            Null 허용여부: true
-            유일값(중복값x): true
+            email, snsId 가 둘다 null 허용으로 되어있다
+            이는 둘중 하나라도 가지고잇으면 되기 때문이다
+            그
             */
-            email: {
-                type: Sequelize.STRING(40),
-                allowNull: true,
-                unique: true,
-            },
-            nick: {
-                type: Sequelize.STRING(15),
-                allowNull: false,
-            },
-            password: {
-                type: Sequelize.STRING(100),
-                allowNull: true,
-            },
-            /*
-            속성명: provider
-            속성 타입: String(단, 'local', 'kakao' 값만 들어갈수있음)
-            null 허용여부: false(허용안함)
-            기본값(추가할때 따로 지정안해놓을시): 'local'
-            */
-            provider: {
-                type: Sequelize.ENUM('local', 'kakao'),
-                allowNull: false,
-                defaultValue: 'local',
-            },
-            snsId: {
-                type: Sequelize.STRING(30),
-                allowNull: true,
-            }
-        }, {
-            // validate
-            sequelize,
-            timestamps: true, //createAt, updatedAt(유저 생성일, 유저 정보 수정일때 자동으로 기록)
-            underscored: false, // true일때 created_at, updated_at으로 변경된다
-            modelName: 'User',
-            tableName: 'users',
-            paranoid: true, // deletedAt 유저 삭제일 -> 삭제일이 있을때 삭제된 데이터 즉, soft delete이ek 
-            charset: 'utf8', //이모티콘 까지 저장하겠다하면 utf8mb4 로 두자 
-            collate: 'utf8_general_ci',
-        })
-    }
-
+            User.init({
+                /*
+                속성명: email
+                속성 타입: String(40자 이내)
+                Null 허용여부: true
+                유일값(중복값x): true
+                */
+                email: {
+                    type: Sequelize.STRING(40),
+                    allowNull: true,
+                    unique: true,
+                },
+                nick: {
+                    type: Sequelize.STRING(15),
+                    allowNull: false,
+                },
+                password: {
+                    type: Sequelize.STRING(100),
+                    allowNull: true,
+                },
+                /*
+                속성명: provider
+                속성 타입: String(단, 'local', 'kakao' 값만 들어갈수있음)
+                null 허용여부: false(허용안함)
+                기본값(추가할때 따로 지정안해놓을시): 'local'
+                */
+                provider: {
+                    type: Sequelize.ENUM('local', 'kakao'),
+                    allowNull: false,
+                    defaultValue: 'local',
+                },
+                snsId: {
+                    type: Sequelize.STRING(30),
+                    allowNull: true,
+                }
+            }, {
+                // validate
+                sequelize,
+                timestamps: true, //createAt, updatedAt(유저 생성일, 유저 정보 수정일때 자동으로 기록)
+                underscored: false, // true일때 created_at, updated_at으로 변경된다
+                modelName: 'User',
+                tableName: 'users',
+                paranoid: true, // deletedAt 유저 삭제일 -> 삭제일이 있을때 삭제된 데이터 즉, soft delete이ek 
+                charset: 'utf8mb4', //이모티콘 까지 저장하겠다하면 utf8mb4 로 두자 
+                collate: 'utf8_general_ci',
+            })
+        }
+        // 관계 관련해서 여기서 한다
     static associations(db) {}
 }
 
