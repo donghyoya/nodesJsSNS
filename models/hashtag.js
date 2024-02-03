6afd3e6b413ea6c7daa17daa811cf282f8038a6c
donghyoya <dongwooya123@gmail.com>
2024-02-03 22:22:14 +0900

    update table setting

--- models/hashtag.js
+++ models/hashtag.js
@@ -2,7 +2,12 @@ const Sequelize = require('sequelize');
 
 class HashTag extends Sequelize.Model {
     static initiate(sequelize) {
-
+        HashTag.init({
+            title: {
+                type: Sequelize.STRING(15),
+                allowNull: false,
+            }
+        })
     }
 
     static associations(db) {}
---
+++ models/user.js
@@ -0,0 +1,74 @@
+const Sequelize = require('sequelize');
+
+class User extends Sequelize.Model {
+    static initiate(sequelize) {
+        /*
+        email, snsId 가 둘다 null 허용으로 되어있다
+        이는 둘중 하나라도 가지고잇으면 되기 때문이다
+        그
+        */
+        User.init({
+            /*
+            속성명: email
+            속성 타입: String(40자 이내)
+            Null 허용여부: true
+            유일값(중복값x): true
+            */
+            email: {
+                type: Sequelize.STRING(40),
+                allowNull: true,
+                unique: true,
+            },
+            nick: {
+                type: Sequelize.STRING(15),
+                allowNull: false,
+            },
+            password: {
+                type: Sequelize.STRING(100),
+                allowNull: true,
+            },
+            /*
+            속성명: provider
+            속성 타입: String(단, 'local', 'kakao' 값만 들어갈수있음)
+            null 허용여부: false(허용안함)
+            기본값(추가할때 따로 지정안해놓을시): 'local'
+            */
+            provider: {
+                type: Sequelize.ENUM('local', 'kakao'),
+                allowNull: false,
+                defaultValue: 'local',
+            },
+            snsId: {
+                type: Sequelize.STRING(30),
+                allowNull: true,
+            }
+        }, {
+            // validate
+            sequelize,
+            timestamps: true, //createAt, updatedAt(유저 생성일, 유저 정보 수정일때 자동으로 기록)
+            underscored: false, // true일때 created_at, updated_at으로 변경된다
+            modelName: 'User',
+            tableName: 'users',
+            paranoid: true, // deletedAt 유저 삭제일 -> 삭제일이 있을때 삭제된 데이터 즉, soft delete이ek 
+            charset: 'utf8', //이모티콘 까지 저장하겠다하면 utf8mb4 로 두자 
+            collate: 'utf8_general_ci',
+        })
+    }
+
+    static associations(db) {}
+}
+
+module.exports = User;
+
+/*
+model의 기본구조 springboot의 entity 부분이라고 생각하면 
+class User extends Sequelize.Model{
+    static initiate(sequelize){
+
+    }
+
+    static associations(db){}
+}
+
+module.exports = User;
+*/
