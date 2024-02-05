fa8133418bf8b429854ec6b70161198269a90bca
donghyoya <dongwooya123@gmail.com>
2024-02-05 10:34:08 +0900

    add middlewares index

--- routes/page.js
+++ routes/page.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { renderJoin, renderMain, renderProfile } = require('../controllers/page');
+const { isLoggedIn } = require('../middlewares');
 
 //아래 라우터들에서 공통적으로 사용가능한 변수를 선언 
 router.use((req, res, next) => {
@@ -12,8 +13,8 @@ router.use((req, res, next) => {
     next(); // 집어넣어야 미들웨어 동작한다
 });
 
-
-router.get('/profile', renderProfile);
+// isLoggedIn은 middlewares안에 있는 index안에 있는 함수이다 
+router.get('/profile', isLoggedIn, renderProfile);
 router.get('/join', renderJoin);
 router.get('/', renderMain);
 
