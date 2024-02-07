4baf064f81c88ff690fcbfdb2a54a2557e0485f2
donghyoya <dongwooya123@gmail.com>
2024-02-07 16:43:21 +0900

    setting routers kakao

--- routes/auth.js
+++ routes/auth.js
@@ -14,6 +14,14 @@ router.post('/login', isNotLoggedIn, login);
 // Get /auth/logout
 router.get('/logout', isLoggedIn, logout);
 
+router.get('/kakao', passport.authenticate('kakao'));
+
+router.get('/kakao/callback', passport.authenticate('kakao', {
+    failureRedirect: '/?loginError=카카오로그인 실패'
+}), (req, res) => {
+    res.redirect('/');
+});
+
 /*
 //local 일때 passport안에 index 파일안에 local로 넘어간다 
 router.post('/auth/login', passport.authenticate('local'), () => {
